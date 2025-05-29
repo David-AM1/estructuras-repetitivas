fcbd6784da3a487500d652bcafebddfd895885cc
David-AM1 <davalosm@unjbg.edu.pe>
2025-05-28 22:57:51 -0500

    Agrega validacion de numeros mayores que cero, añade uso de tildes y se muestra promedio de multiplos en el EJERCICIO 1 (repetitivas)

--- 1. multiplos.cpp
+++ 1. multiplos.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main() {
-    int i, multiplo, numero;
-    cout<<"Ingrese la cantidad de multiplos de 7 que necesita: ";
+	SetConsoleOutputCP(CP_UTF8);
+    int i, suma=0;
+    double promedio, multiplo, numero;
+    do{
+    cout<<"Ingrese la cantidad de múltiplos de 7 que necesita: ";
     cin>>numero;
+    if(numero<=0){
+    	cout<<"Por favor ingrese un número mayor que cero."<<endl<<endl;
+	}
+	} while (numero<=0);
     multiplo=0;
-    cout<<"Los primeros "<<numero<<" multiplos de 7 son: "<<endl;
+    cout<<"Los primeros "<<numero<<" múltiplos de 7 son: "<<endl;
     for (i=1;i<=numero;i++) {
         multiplo=7*i;
+        suma=suma+multiplo;
         cout<<i<<". "<<multiplo<<endl;
     }
+    promedio=suma/numero;
+    cout<<"El promedio de los multiplos es "<<promedio;
     return 0;
 }
