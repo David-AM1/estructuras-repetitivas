a8486760f6498b81af6aae72c45a632c4670a8f5
David-AM1 <davalosm@unjbg.edu.pe>
2025-05-29 23:34:21 -0500

    Se agrega ennumercion en orden de los numeros digitados y se muestra la suma de numeros positivos en el EJERCICIO 2

--- 2. solo positivos.cpp
+++ 2. solo positivos.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int numero;
+	int numero, suma=0, i=1;
 	cout<<"Digite numeros (Excepto negativos): "<<endl;
 	do{
+	cout<<i<<". ";
 	cin>>numero;
+	i++;
+	if(numero>=0){
+		suma+=numero;
+	}
 	}while (numero>=0);
+	cout<<"La suma de numeros digitados (positivos) es "<<suma<<endl;
 	cout<<"Fin de la captura.";
 	return 0;
 }
