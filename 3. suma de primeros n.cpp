ef449771f4f918e12533b097df97f7531ee4cf02
David-AM1 <davalosm@unjbg.edu.pe>
2025-05-30 23:54:03 -0500

    Se añade reintento si el numero es menor o igual a cero y la verificacion si la suma de numeros es par o impar en el EJERCICIO 3

--- 3. suma de primeros n.cpp
+++ 3. suma de primeros n.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int i, numero, suma;
+	int i, numero, suma, impar;
+	do{
 	cout<<"Ingrese un numero: "; 
 	cin>>numero;
+	if(numero<=0){
+		cout<<"Intente nuevamente ingresando un numero mayor que cero."<<endl;
+	}
+	}while(numero<=0);
 	suma=0;
 	for (i=1;i<=numero;i++){
 		suma+=i;
 	}
-	cout<<"La suma de los primeros "<<numero<<" numeros es: "<<suma;
+	cout<<"La suma de los primeros "<<numero<<" numeros es: "<<suma<<endl;
+	if(suma%2==0){
+		cout<<"Dato: la suma de los numeros es un numero par.";
+	}else{
+		cout<<"Dato: la suma de los numeros es un numero impar.";
+	}
 	return 0;
 }
